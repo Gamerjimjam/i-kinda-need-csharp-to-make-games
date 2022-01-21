1e12229b182832d78985c262f6bd1fddd837d856
Gamerjimjam <85574259+Gamerjimjam@users.noreply.github.com>
2022-01-21 18:26:19 -0500

    magik

---
+++ cshap/Class2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace penis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("type in sum");
+            string value = Console.ReadLine();
+            Console.WriteLine( "did you type this:" + value);
+        }
+    }
+}
