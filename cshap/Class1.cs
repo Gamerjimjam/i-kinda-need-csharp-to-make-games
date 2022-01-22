a44fa8594435527c04cc85066203d8338bf8b234
Gamerjimjam <85574259+Gamerjimjam@users.noreply.github.com>
2022-01-22 11:50:00 -0500

    fr

---
+++ cshap/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace csmath
+{
+    class program
+    {
+        static void main(string[] args)
+        {
+            int x = 48;
+            int y = 28;
+            if (x == y)
+            {
+                Console.WriteLine("equal");
+            }
+            else if (y >= x)
+            {
+                Console.WriteLine("y is larger then x");
+            }
+            else
+            {
+                Console.WriteLine("x is bigger then y or other ");
+            }
+        }
+    }
+}
---
+++ cshap/Class3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace penis
+{
+    class Program
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("type in sum");
+            string value = Console.ReadLine();
+            Console.WriteLine("did you type this: \n" + value);
+
+            Console.WriteLine("hey kiddie whats your age :)))");
+            string age = Console.ReadLine();
+            int paris = Convert.ToInt32(age);
+            if (paris < 18)
+            {
+                Console.WriteLine("shawty looking bad asf tho");
+            }
+            else if (paris == 12)
+            {
+                Console.WriteLine("ZAMN");
+            }
+            else { Console.WriteLine("zad ;'("); }
+        }
+    }
+}
