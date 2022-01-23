9d10f41a00db962700e583bc05d32d41c954398b
Gamerjimjam <85574259+Gamerjimjam@users.noreply.github.com>
2022-01-23 11:07:35 -0500

    average youtuber

---
+++ cshap/video.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+    
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace video
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("are you subbed?");
+            string result = Console.ReadLine();
+            if (result == "yes")
+            {
+                Console.WriteLine("thank you");
+            } else if (result == "no")
+            { 
+                Console.WriteLine("thank you for not subscribing, it is a big advantage");
+            } else if (result == null) { 
+                Console.WriteLine("you good"); 
+            }
+        }
+    }
+}
